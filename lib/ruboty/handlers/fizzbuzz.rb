c25654a6c3a6bdc7f1029d337f497f7412e5713c
javel <javel555@gmail.com>
2019-01-06 06:25:21 +0900

    add fortytwo

--- lib/ruboty/handlers/fizzbuzz.rb
+++ lib/ruboty/handlers/fizzbuzz.rb
@@ -10,6 +10,11 @@ module Ruboty
         Ruboty::LookAtMe::Actions::Fizzbuzz.new(message).call
       end
 
+      on /生命、宇宙、そして万物についての究極の疑問の答え/, name: 'fortytwo', description: 'Answer to the Ultimate Question of Life, the Universe, and Everything'
+      def fortytwo(message)
+        message.reply('42')
+      end
+
     end
 
   end
